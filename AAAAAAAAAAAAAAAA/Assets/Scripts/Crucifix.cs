42fc52945930cac40195491ae511f8238dd47f7a
skyler kim <konata.izumi20@gmail.com>
2024-11-23 23:55:23 -0500

    Add chris image

---
+++ AAAAAAAAAAAAAAAA/Assets/Scripts/Crucifix.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crucifix : MonoBehaviour
+{
+    public float crucifixTime = 0f;
+    public float maxCrucifixTime = 7f;
+    public float timeToCrucificTime = 0.5f;
+    public Animator anim;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
